8930ed5d52ca3a076088fbb2dc45e2d7018d2e59
Aleksey Kazarin <aleksey.ka@livetex.ru>
2016-11-24 15:14:22 +0300

    remove commented lines

--- src/index.js
+++ src/index.js
@@ -6,18 +6,9 @@ function MyReporter(runner) {
   var passes = 0;
   var failures = 0;
 
-  runner.on('pass', function(test){
-    passes++;
-    console.log('pass: %s', test.fullTitle());
-  });
-
-  runner.on('fail', function(test, err){
+  runner.on('fail', function(test){
     failures++;
     console.log('FAILEDTEST: %s', test.title);
   });
 
-  /*runner.on('end', function(){
-    console.log('111111 end: %d/%d', passes, passes + failures);
-    process.exit(failures);
-  });*/
 }
