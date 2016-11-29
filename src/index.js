ecc466ed13b72ea9f75ca931a367e327e2345675
Aleksey Kazarin <aleksey.ka@livetex.ru>
2016-11-29 13:51:06 +0300

    add pass, pending, test end blocks and end block with statistic result

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ module.exports = MyReporter;
 
 function MyReporter(runner) {
   mocha.reporters.Base.call(this, runner);
+  var summarises = 0;
   var passes = 0;
+  var pendings = 0;
   var failures = 0;
+
+  runner.on('test end', function(test) {
+    summarises++
+    console.log('TESTEND: %s', test.title);
+  });
+
+  runner.on('pass', function(test) {
+    passes++;
+    console.log('PASSEDTEST: %s', test.title);
+  });
+
+  runner.on('pending', function(test) {
+    pendings++;
+    console.log('PENDINGTEST: %s', test.title);
+  });
 
   runner.on('fail', function(test){
     failures++;
     console.log('FAILEDTEST: %s', test.title);
   });
 
-}
+  runner.on('end', function() {
+    console.log('STATISTICS: pass: %s; pending: %s; fail: %s; all: %s', passes, pendings, failures, summarises);
+  });
+
+};
