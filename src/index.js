1352aaa410ada0cc790198314a86d9cb7bad10ce
Aleksey Kazarin <aleksey.ka@livetex.ru>
2016-11-29 15:39:48 +0300

    add debug info about tmp count results

--- src/index.js
+++ src/index.js
@@ -11,21 +11,25 @@ function MyReporter(runner) {
   runner.on('test end', function(test) {
     summarises++
     console.log('TESTEND: %s', test.title);
+    console.log('RESULT1: pass: %s; pending: %s; fail: %s; all: %s', passes, pendings, failures, summarises);
   });
 
   runner.on('pass', function(test) {
     passes++;
     console.log('PASSEDTEST: %s', test.title);
+    console.log('RESULT2: pass: %s; pending: %s; fail: %s; all: %s', passes, pendings, failures, summarises);
   });
 
   runner.on('pending', function(test) {
     pendings++;
     console.log('PENDINGTEST: %s', test.title);
+    console.log('RESULT3: pass: %s; pending: %s; fail: %s; all: %s', passes, pendings, failures, summarises);
   });
 
   runner.on('fail', function(test){
     failures++;
     console.log('FAILEDTEST: %s', test.title);
+    console.log('RESULT3: pass: %s; pending: %s; fail: %s; all: %s', passes, pendings, failures, summarises);
   });
 
   runner.on('end', function() {
